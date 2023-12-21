7a651c80ea00642f005c37be36833be9ff61db46
Alex Waibel <alexwaibel@users.noreply.github.com>
2023-12-21 03:19:59 -0800

    Remove broken git plugin and unused func in main

--- main.gd
+++ main.gd
@@ -5,10 +5,6 @@ func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
