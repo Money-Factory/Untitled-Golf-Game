0d0a5e4c7d39b4902ed8377c3d95db16c5c0e9ac
Derek Boyer <20boyerd@gmail.com>
2023-12-31 01:47:14 -0800

    fixed movment names in player.gs

--- player.gd
+++ player.gd
@@ -17,7 +17,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var input_dir: Vector2 = Input.get_vector("left", "right", "up", "down")
+	var input_dir: Vector2 = Input.get_vector("left", "right", "forward", "back")
 	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
